29884c4b73990e2d284ce2e0744ab5ebc90a3331
unknown <csengineer13@gmail.com>
2016-01-10 15:13:36 -0500

    Init application

---
+++ Simple.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simple.MVC.Startup))]
+namespace Simple.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
