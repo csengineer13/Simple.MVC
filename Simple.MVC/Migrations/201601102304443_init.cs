e1d9ab17943b3bbba8893e22bb0c774daef95b85
unknown <csengineer13@gmail.com>
2016-01-10 18:21:17 -0500

    Initial DB Migrations

---
+++ Simple.MVC/Migrations/201601102304443_init.cs
@@ -0,0 +1,110 @@
+namespace Simple.MVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Test",
+                c => new
+                    {
+                        Id = c.Guid(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserClaim",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ClaimType = c.String(maxLength: 150),
+                        ClaimValue = c.String(maxLength: 500),
+                        User_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.User", t => t.User_Id)
+                .Index(t => t.User_Id);
+            
+            CreateTable(
+                "dbo.User",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        UserName = c.String(nullable: false),
+                        PasswordHash = c.String(maxLength: 500),
+                        SecurityStamp = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UserLogin",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.LoginProvider, t.ProviderKey })
+                .ForeignKey("dbo.User", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.UserRole",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId)
+                .ForeignKey("dbo.User", t => t.UserId)
+                .Index(t => t.RoleId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.User", t => t.Id)
+                .Index(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUsers", "Id", "dbo.User");
+            DropForeignKey("dbo.UserClaim", "User_Id", "dbo.User");
+            DropForeignKey("dbo.UserRole", "UserId", "dbo.User");
+            DropForeignKey("dbo.UserRole", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.UserLogin", "UserId", "dbo.User");
+            DropIndex("dbo.AspNetUsers", new[] { "Id" });
+            DropIndex("dbo.UserClaim", new[] { "User_Id" });
+            DropIndex("dbo.UserRole", new[] { "UserId" });
+            DropIndex("dbo.UserRole", new[] { "RoleId" });
+            DropIndex("dbo.UserLogin", new[] { "UserId" });
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.UserRole");
+            DropTable("dbo.UserLogin");
+            DropTable("dbo.User");
+            DropTable("dbo.UserClaim");
+            DropTable("dbo.Test");
+            DropTable("dbo.AspNetRoles");
+        }
+    }
+}
