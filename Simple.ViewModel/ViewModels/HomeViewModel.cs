be4c3d6df346001f0cf24eac0c4347a230e86853
unknown <csengineer13@gmail.com>
2016-01-10 21:21:14 -0500

    Beginning to hammer out an initial ViewModel for the home screen

---
+++ Simple.ViewModel/ViewModels/HomeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple.ViewModel.ViewModels
+{
+	public class HomeViewModel : ViewModelBase<HomeDto, HomeDetailDto, Guid>
+	{
+		public HomeViewModel()
+		{
+			Title = "Home";
+		}
+
+		public override Guid GetDefaultIdValue()
+		{
+			return new Guid();
+		}
+
+		public HomeViewModelMeta Meta { get; set; }
+	}
+}
