cd5b258589b80e82e3a663379ce6bde52b346aeb
unknown <csengineer13@gmail.com>
2016-01-10 20:42:21 -0500

    Adding KOMapping

---
+++ Simple.ViewModel/Common/KOMapping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple.ViewModel.Common
+{
+	public class KOMapping
+	{
+		public KOMapping()
+		{
+			// Always init lists in KO ;)
+			ignore = new List<string>();	
+			ignorePostBack = new List<string>();
+			copy = new List<string>() { "KOMapping.copy", "Meta"}; // Forces meta to copy as non observable
+		}
+
+		public List<string> ignore { get; set; }
+		public List<string> copy { get; set; }
+		public List<string> ignorePostBack { get; set; }
+		public List<string> allNonPostBack
+		{
+			get { return copy.Union(ignorePostBack).ToList();  }
+		} 
+	}
+}
