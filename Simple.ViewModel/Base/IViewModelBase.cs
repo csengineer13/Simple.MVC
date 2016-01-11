7133c8d43e9384cf103537cabdbfd46c96570751
unknown <csengineer13@gmail.com>
2016-01-10 21:20:51 -0500

    Adding an interface that more or less mimics the matching abstract class

---
+++ Simple.ViewModel/Base/IViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Simple.ViewModel.Common;
+using Simple.ViewModel.DTO;
+using Simple.ViewModel.Interfaces;
+
+namespace Simple.ViewModel.Base
+{
+	public interface IViewModelBase<Dto, DDto, TId>
+		where Dto : class, IDto<TId>, new()
+		where DDto : class, IDto<TId>, new()
+	{
+		List<Dto> List { get; set; } // List || Table
+		SimpleModelState ModelState { get; set; }
+		Dto Item { get; set; }		  
+		DDto ItemDetail { get; set; } // Form || Selected
+		List<ColumnData> Columns { get; }
+		KOMapping KOMapping { get; set; }
+		string ToJson();
+		bool ItemDetailIsNew { get; set; }
+		string Title { get; set; }
+		LoggedInUserDTO User { get; set; }
+		PageInfoDTO PageInfo { get; set; }
+
+	}
+}
