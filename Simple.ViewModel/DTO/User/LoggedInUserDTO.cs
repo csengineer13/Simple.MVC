ef38146cd09bac264e4ff9acc4761ed5cc990e38
unknown <csengineer13@gmail.com>
2016-01-10 20:56:52 -0500

    Adding a VM representation of our loggedInUser

---
+++ Simple.ViewModel/DTO/User/LoggedInUserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Simple.ViewModel.DTO.User
+{
+	public class LoggedInUserDTO
+	{
+		public Guid Id { get; set; }
+		public string UserName { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+	}
+}
