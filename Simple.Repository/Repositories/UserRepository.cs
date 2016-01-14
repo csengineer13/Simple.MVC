1a58a0294e9447076bb4b74ea953bb1c4e78365d
unknown <csengineer13@gmail.com>
2016-01-14 11:09:24 -0500

    Implementations for our other repositories

---
+++ Simple.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using Simple.Domain.Entities;
+using Simple.Domain.Repositories;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple.Repository.Repositories
+{
+	internal class UserRepository : Repository<User>, IUserRepository
+	{
+		internal UserRepository(ApplicationDbContext context)
+			: base(context)
+		{
+		}
+
+		public User FindByUserName(string username)
+		{
+			return Set.FirstOrDefault(x => x.UserName == username);
+		}
+
+		public Task<User> FindByUserNameAsync(string username)
+		{
+			return Set.FirstOrDefaultAsync(x => x.UserName == username);
+		}
+
+		public Task<User> FindByUserNameAsync(System.Threading.CancellationToken cancellationToken, string username)
+		{
+			return Set.FirstOrDefaultAsync(x => x.UserName == username, cancellationToken);
+		}
+
+		public User FindByEmail(string username)
+		{
+			return Set.FirstOrDefault(x => x.UserName == username);
+		}
+
+		public Task<User> FindByEmailAsync(string username)
+		{
+			return Set.FirstOrDefaultAsync(x => x.UserName == username);
+		}
+
+		public Task<User> FindByEmailAsync(System.Threading.CancellationToken cancellationToken, string username)
+		{
+			return Set.FirstOrDefaultAsync(x => x.UserName == username, cancellationToken);
+		}
+	}
+}
