802ed3de3f7a5e8cef6cb49cd7b3674699de38c9
unknown <csengineer13@gmail.com>
2016-01-14 12:02:46 -0500

    Setup

--- Simple.MVC/App_Start/UnityConfig.cs
+++ Simple.MVC/App_Start/UnityConfig.cs
@@ -1,21 +1,25 @@
-using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Microsoft.Practices.Unity;
+using Simple.Repository;
+using Simple.Domain;
+using Simple.MVC.Identity;
+using System;
+using System.Web.Mvc;
 using Unity.Mvc5;
 
 namespace Simple.MVC
 {
-    public static class UnityConfig
-    {
-        public static void RegisterComponents()
-        {
+	public static class UnityConfig
+	{
+		public static void RegisterComponents()
+		{
 			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
-            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
-        }
-    }
+
+			container.RegisterType<IUnitOfWork, UnitOfWork>(new HierarchicalLifetimeManager(), new InjectionConstructor("SimpleMVCConnectionString"));
+			container.RegisterType<IUserStore<IdentityUser, Guid>, UserStore>(new TransientLifetimeManager());
+			container.RegisterType<RoleStore>(new TransientLifetimeManager());
+
+			DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+		}
+	}
 }
