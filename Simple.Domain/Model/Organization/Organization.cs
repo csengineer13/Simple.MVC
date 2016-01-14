250f8b09ce9440092100d3f3131c5dd7c41028c2
unknown <csengineer13@gmail.com>
2016-01-14 10:45:26 -0500

    Adding organization

---
+++ Simple.Domain/Model/Organization/Organization.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Simple.Domain.Model
+{
+	public class Organization
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string CatchPhrase { get; set; }
+		public string BSPhrase { get; set; }
+
+		// Ref
+		public DomainUser Owner { get; set; }
+
+		// List Ref
+		public List<Address> Addresses { get; set; }
+		public List<DomainUser> Employees { get; set; }
+	}
+}
