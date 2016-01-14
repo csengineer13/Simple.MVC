688d4058b83d49c20d4900c3fd03c61b1648042d
unknown <csengineer13@gmail.com>
2016-01-13 23:50:51 -0500

    Removing unused actions

--- Simple.MVC/Controllers/HomeController.cs
+++ Simple.MVC/Controllers/HomeController.cs
@@ -10,19 +10,5 @@ namespace Simple.MVC.Controllers
 			var vm = new HomeViewModel();
 			return View(vm);
 		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
 	}
 }
