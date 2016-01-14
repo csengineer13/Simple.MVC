0a084e70c166da61d424ff340552ad65f002589f
unknown <csengineer13@gmail.com>
2016-01-14 12:16:06 -0500

    Making it possible to generate migrations

--- Simple.Repository/ApplicationDbContext.cs
+++ Simple.Repository/ApplicationDbContext.cs
@@ -1,19 +1,21 @@
 ﻿using Simple.Repository.Configuration;
 using Simple.Domain.Entities;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace Simple.Repository
 {
-	internal class ApplicationDbContext : DbContext
+	// 
+	public class ApplicationDbContext : DbContext
 	{
-		internal ApplicationDbContext(string nameOrConnectionString)
+		public ApplicationDbContext(string nameOrConnectionString)
 			: base(nameOrConnectionString)
 		{
 		}
 
-		internal IDbSet<User> Users { get; set; }
-		internal IDbSet<Role> Roles { get; set; }
-		internal IDbSet<ExternalLogin> Logins { get; set; }
+		public IDbSet<User> Users { get; set; }
+		public IDbSet<Role> Roles { get; set; }
+		public IDbSet<ExternalLogin> Logins { get; set; }
 
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
@@ -21,6 +23,14 @@ namespace Simple.Repository
 			modelBuilder.Configurations.Add(new RoleConfiguration());
 			modelBuilder.Configurations.Add(new ExternalLoginConfiguration());
 			modelBuilder.Configurations.Add(new ClaimConfiguration());
+		}
+	}
+
+	public class MigrationsContextFactory : IDbContextFactory<ApplicationDbContext>
+	{
+		public ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext("SimpleMVCConnectionString");
 		}
 	}
 }
