7370d5d3f749b86730d47f227683a668ab4fff13
unknown <csengineer13@gmail.com>
2016-01-13 22:18:07 -0500

    Set "claims" for fetching LoggedInUserDTO without a DB call on each query

---
+++ Simple.MVC/Common/CustomIdentityExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNet.Identity;
+using Simple.ViewModel.DTO;
+
+namespace Simple.MVC.Common
+{
+	public static class CustomIdentityExtensions
+	{
+		public static LoggedInUserDTO GetIdentityLoggedInUserDto(this IIdentity identity)
+		{
+			if (identity == null)
+				return null;
+
+			var user = new LoggedInUserDTO
+			{
+				Id = Guid.Parse(identity.GetUserId()), // Could blow up
+				UserName = identity.GetUserName(),
+				FirstName = (identity as ClaimsIdentity).FirstOrNull(ClaimTypes.GivenName),
+				LastName = (identity as ClaimsIdentity).FirstOrNull(ClaimTypes.Surname)
+			};
+
+			return user;
+		}
+
+		internal static string FirstOrNull(this ClaimsIdentity identity, string claimType)
+		{
+			var val = identity.FindFirst(claimType);
+
+			return val == null ? null : val.Value;
+		}
+	}
+}
