099d7962e6e897781d239584fdd948f787cfcd4b
unknown <csengineer13@gmail.com>
2016-01-13 22:17:15 -0500

    Get all of our user properties into the same table

--- Simple.MVC/simpleDbContext.cs
+++ Simple.MVC/simpleDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Simple.Domain.Model;
@@ -10,15 +8,14 @@ using Simple.Domain.Model;
 namespace Simple.MVC
 {
 	// You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-	public class ApplicationUser : IdentityUser
+	public class User : IdentityUser
 	{
-		// Custom properties
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Email { get; set; }
 	}
-
-	public class ApplicationDbContext : IdentityDbContext<ApplicationUser> //IdentityDbContext<User, Role, Guid, UserLogin, UserRole, UserClaim>
+	 //public class ApplicationDbContext : IdentityDbContext<ApplicationUser, CustomRole, Guid, CustomUserLogin, CustomUserRole, CustomUserClaim>
+	public class ApplicationDbContext : IdentityDbContext<User> //IdentityDbContext<User, Role, Guid, UserLogin, UserRole, UserClaim>
 	{
 		public ApplicationDbContext()
 			: base("DefaultConnection")
@@ -52,6 +49,7 @@ namespace Simple.MVC
 
 			// Configure Asp Net Identity Tables
 			modelBuilder.Entity<IdentityUser>().ToTable("User");
+			modelBuilder.Entity<User>().ToTable("User");
 			modelBuilder.Entity<IdentityUser>().Property(u => u.PasswordHash).HasMaxLength(500);
 			//modelBuilder.Entity<IdentityUser>().Property(u => u.Stamp).HasMaxLength(500);
 			//modelBuilder.Entity<IdentityUser>().Property(u => u.PhoneNumber).HasMaxLength(50);
