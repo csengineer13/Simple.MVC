d87044b03bd39922c7e2976092a2d1dafdcc847e
unknown <csengineer13@gmail.com>
2016-01-14 15:05:26 -0500

    Default login screen

--- Simple.MVC/App_Start/Startup.Auth.cs
+++ Simple.MVC/App_Start/Startup.Auth.cs
@@ -14,7 +14,7 @@ namespace Simple.MVC
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login")
+                LoginPath = new PathString("/")
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
