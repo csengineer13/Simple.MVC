f7c2eee8350f1e2e2b1fac0b155ca8cf6b5ffa6c
unknown <csengineer13@gmail.com>
2016-01-14 11:01:54 -0500

    Adding unitOfWork interface

---
+++ Simple.Domain/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Simple.Domain.Repositories;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Simple.Domain
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		#region Properties
+		IExternalLoginRepository ExternalLoginRepository { get; }
+		IRoleRepository RoleRepository { get; }
+		IUserRepository UserRepository { get; }
+		#endregion
+
+		#region Methods
+		int SaveChanges();
+		Task<int> SaveChangesAsync();
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+		#endregion
+	}
+}
